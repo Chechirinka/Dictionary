a82fb95a8904d952e57267152971c5b5f66cbe2f
Checherinka <irina.ch.11@mail.ru>
2022-08-04 16:06:49 +0300

    Fix bug

--- src/main/java/dictionarySpring/dao/DictionaryDAO.java
+++ src/main/java/dictionarySpring/dao/DictionaryDAO.java
@@ -16,27 +16,68 @@ public class DictionaryDAO implements DictionaryStorage {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    private static final String SEARCH = "SELECT key, value " +
+            "FROM( " +
+            "SELECT dictionaries.id AS m, words.word AS key, dictionaries.keys, languages.name AS lan_first " +
+            "FROM " +
+            "languages  JOIN words ON languages.id = words.lan_id " +
+            "JOIN dictionaries ON words.id = dictionaries.keys " +
+            ") AS a " +
+            "JOIN (SELECT dictionaries.id AS l, words.word AS value, dictionaries.values, languages.name As lan_second " +
+            "FROM " +
+            "languages  JOIN words ON languages.id = words.lan_id " +
+            "JOIN dictionaries ON words.id = dictionaries.values) AS c " +
+            "ON a.m = c.l " +
+            "WHERE key = ? AND lan_first = ? AND lan_second = ? ";
+
+    private static final String READ = "SELECT key, value " +
+            "FROM( " +
+            "SELECT dictionaries.id AS m, words.word AS key, dictionaries.keys, languages.name AS lan_first " +
+            "FROM " +
+            "languages  JOIN words ON languages.id = words.lan_id " +
+            "JOIN dictionaries ON words.id = dictionaries.keys " +
+            ") AS a " +
+            "JOIN (SELECT dictionaries.id AS l, words.word AS value, dictionaries.values, languages.name As lan_second " +
+            "FROM " +
+            "languages  JOIN words ON languages.id = words.lan_id " +
+            "JOIN dictionaries ON words.id = dictionaries.values) AS c " +
+            "ON a.m = c.l " +
+            "WHERE lan_first = ? AND lan_second = ? ";
+
+    private static final String DELETING = "DELETE FROM dictionaries " +
+            "WHERE keys = (SELECT id FROM words WHERE word = ?) or values = (SELECT id FROM words WHERE word = ?); " +
+            "DELETE FROM words WHERE word = ?";
+
+    private static final String ADDING = "insert into words(word, lan_id) " +
+            "values (?, (select id FROM languages where name = ?)); " +
+            "insert into words(word, lan_id) " +
+            "values (?, (select id FROM languages where name = ?)); ";
+
     @Override
     public List<DictionaryLine> read(DictionaryType selectedDictionary) {
-        return jdbcTemplate.query("SELECT * FROM dictline", new BeanPropertyRowMapper<>(DictionaryLine.class));
+        return jdbcTemplate.query(READ, new Object[]{selectedDictionary.getTo(), selectedDictionary.getFrom()}, new BeanPropertyRowMapper<>(DictionaryLine.class));
     }
 
     @Override
     public boolean addTo(String key, String value, DictionaryType selectedDictionary) {
-        jdbcTemplate.update("INSERT INTO dictline(key, value) VALUES(?, ?)", key, value);
+        jdbcTemplate.update(ADDING, key, selectedDictionary.getFrom(), value, selectedDictionary.getTo());
         return true;
     }
 
     @Override
     public boolean remove(String key, DictionaryType selectedDictionary) {
-        jdbcTemplate.update("DELETE FROM dictline WHERE key = ?", key);
+        jdbcTemplate.update(DELETING, key, key, key);
         return true;
     }
 
     @Override
     public DictionaryLine search(String key, DictionaryType selectedDictionary) {
-
-        return jdbcTemplate.query("SELECT * FROM dictline WHERE key = ?", new Object[]{key}, new BeanPropertyRowMapper<>(DictionaryLine.class))
-                .stream().findAny().orElse(null);
+        List<DictionaryLine> searchLines = jdbcTemplate.query(SEARCH, new Object[]{key, selectedDictionary.getFrom(), selectedDictionary.getTo()}, new BeanPropertyRowMapper<>(DictionaryLine.class));
+        for (DictionaryLine searchLine : searchLines) {
+            if (key.equals(searchLine.getKey())) {
+                return searchLine;
+            }
+        }
+        return null;
     }
 }
