2cbb4fcb16af98f87e539b011ef46a86713c6b03
Checherinka <irina.ch.11@mail.ru>
2022-08-10 11:21:20 +0300

    Fix bug
    Добавила Дао классы

---
+++ src/main/java/dictionarySpring/dao/DictionariesDAO.java
@@ -0,0 +1,4 @@
+package dictionarySpring.dao;
+
+public class DictionariesDAO {
+}
--- src/main/java/dictionarySpring/dao/DictionaryJpaHql.java
+++ src/main/java/dictionarySpring/dao/DictionaryJpaHql.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DictionaryJpaHql implements DictionaryStorage {
-    
+
     private SessionFactory sessionFactory;
 
 
