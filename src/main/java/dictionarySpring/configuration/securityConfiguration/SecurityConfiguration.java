e047c09df0a54c1bf5254bf72f51a6f19db0e72e
Checherinka <irina.ch.11@mail.ru>
2022-09-05 12:28:48 +0300

    Add Security

---
+++ src/main/java/dictionarySpring/configuration/securityConfiguration/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package dictionarySpring.configuration.securityConfiguration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider();
+    }
+}
