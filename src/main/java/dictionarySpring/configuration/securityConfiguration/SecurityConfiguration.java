f300f36e64ccf391241886c9d4107fe9f474ca57
Checherinka <irina.ch.11@mail.ru>
2022-09-06 12:19:43 +0300

    Fix bug

--- src/main/java/dictionarySpring/configuration/securityConfiguration/SecurityConfiguration.java
+++ src/main/java/dictionarySpring/configuration/securityConfiguration/SecurityConfiguration.java
@@ -1,14 +1,14 @@
 package dictionarySpring.configuration.securityConfiguration;
 
+import dictionarySpring.security.AuthProviderImpl;
 import dictionarySpring.service.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -22,10 +22,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
---
+++ src/main/java/dictionarySpring/configuration/securityConfiguration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package dictionarySpring.configuration.securityConfiguration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
