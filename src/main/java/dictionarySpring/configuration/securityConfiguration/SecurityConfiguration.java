787e6b591c2e064d718a2a975fd2c10c29452913
Checherinka <irina.ch.11@mail.ru>
2022-09-08 12:19:06 +0300

    Update SecurityConfiguration.java

--- src/main/java/dictionarySpring/configuration/securityConfiguration/SecurityConfiguration.java
+++ src/main/java/dictionarySpring/configuration/securityConfiguration/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package dictionarySpring.configuration.securityConfiguration;
 
-import dictionarySpring.service.PersonDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import dictionarySpring.security.AuthProviderImpl;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,15 +10,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final PersonDetailsService personDetailsService;
+    private final AuthProviderImpl authProvider;
 
-    @Autowired
-    public SecurityConfiguration(PersonDetailsService personDetailsService) {
-        this.personDetailsService = personDetailsService;
+    public SecurityConfiguration(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+        auth.authenticationProvider(authProvider);
     }
+
 }
