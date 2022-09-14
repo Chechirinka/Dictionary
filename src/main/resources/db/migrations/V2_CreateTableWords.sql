9702733af75b20639a38a548ef022d58a71666db
Checherinka <irina.ch.11@mail.ru>
2022-09-14 17:13:22 +0300

    Обновила миграции

--- src/main/resources/db/migrations/V2_CreateTableWords.sql
+++ src/main/resources/db/migrations/V2_CreateTableWords.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.words
 (
-    id bigint NOT NULL DEFAULT nextval('words_id_seq'::regclass),
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     word character varying(39) COLLATE pg_catalog."default" NOT NULL,
     lan_id bigint NOT NULL,
     CONSTRAINT words_pkey PRIMARY KEY (id),
