81ed82003b06603436a1c3d689da969f884cec5a
Checherinka <irina.ch.11@mail.ru>
2022-09-22 15:06:45 +0300

    Update ActionControllerMvc.java

--- src/main/java/dictionarySpring/controllers/ActionControllerMvc.java
+++ src/main/java/dictionarySpring/controllers/ActionControllerMvc.java
@@ -1,8 +1,5 @@
 package dictionarySpring.controllers;
 
-import dictionarySpring.exception.TypeNotFoundException;
-import dictionarySpring.model.dictionaryType.DictionaryType;
-import dictionarySpring.model.modelDefault.DictionaryLine;
 import dictionarySpring.service.DictionaryService;
 import dictionarySpring.service.Formation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -31,7 +27,6 @@ public class ActionControllerMvc {
 
     private Formation formation;
 
-    private ModelAndView modelAndView;
 
     @Autowired
     public ActionControllerMvc(DictionaryService dictionaryService, Formation formation) {
